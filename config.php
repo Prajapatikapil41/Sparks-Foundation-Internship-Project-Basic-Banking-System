4587e54b6237e6f01066789939bd4100433691ea
Kapil Prajapati <kapilpcmc19@svvv.edu.in>
2021-09-21 19:52:41 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+ $dbhost = "localhost";
+ $dbuser = "root";
+ $dbpass = "";
+ $db = "spark";
+ $conn = mysqli_connect($dbhost, $dbuser, $dbpass,$db);
+//  if($conn) {
+//      echo "connection successfull";
+//  }
+//  else {
+//      "<script>alert('connection unsuccessful') </script>";
+//  }
+?>
